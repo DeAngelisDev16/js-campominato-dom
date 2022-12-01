c6998eb850d535c978094a8c7ce5cef1ea7eb762
Shelbot77 <alessiodeangelis021@gmail.com>
2022-12-01 15:41:52 +0100

    upload campominato grid

---
+++ myscript.js
@@ -0,0 +1,21 @@
+const mainContainer = document.getElementById('wrapper');
+const playButton = document.getElementById('play');
+
+
+
+playButton.addEventListener("click", function(){
+    mainContainer.innerHTML= '';
+    for (let i = 1; i <= 100; i++){
+        
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('square');
+        newDiv.addEventListener("click", function(){
+            newDiv.classList.toggle('click');
+            console.log(newDiv);
+        })  
+        
+        mainContainer.append(newDiv);
+        newDiv.append(i);
+    }
+    
+})
