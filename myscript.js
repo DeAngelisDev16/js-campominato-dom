b809c0da366d82852bea5604d13a7eecc6e8daad
Shelbot77 <alessiodeangelis021@gmail.com>
2022-12-01 16:13:53 +0100

    Inserimento funzioni per calcolo numero randomico

--- myscript.js
+++ myscript.js
@@ -19,3 +19,38 @@ playButton.addEventListener("click", function(){
     }
     
 })
+
+// 1)Generare 16 numeri casuali da 1 a 100:
+  //-Preparo un array vuoto, dove inserirò poi i numeri corrispondenti alle bombe:
+  const bombList = [];
+  
+  //Genero un numero casuale tra 1 e 100:
+      //controllo se il numero è già stato generato;
+      //se no =====> aggiungo numero al contenitore (bombList);
+
+  //Se il contenitore (bombList) contiene 16 valori, ho finito;
+  //altrimento torno al punto 1;  
+
+
+//funzione per calcolo numero randomico:  
+function getRandomNumber(numMin, numMax){
+    return Math.floor(Math.random () * (numMax - numMin + 1)+ numMin);
+}
+  
+  
+  
+  //funzione per numero randomico unico:
+  function numeroRandomicoUnico (blacklist, min, max) {
+    let isValid = false;
+    let randomNum;
+
+    while(isValid===false) {
+        randomNum = getRandomNumber(min, max);
+
+        if(!blacklist.includes(randomnum)){
+            isValid = true;
+        }
+       
+       return randomNum;
+    }
+  }
